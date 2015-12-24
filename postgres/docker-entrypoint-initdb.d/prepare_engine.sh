40dd1b0fe919ac423462bc9c60711818b6447e02
Andrey Chernih <andrey.chernih@gmail.com>
2015-12-23 18:43:14 -0800

    Create test DB

--- postgres/docker-entrypoint-initdb.d/prepare_engine.sh
+++ postgres/docker-entrypoint-initdb.d/prepare_engine.sh
@@ -4,5 +4,7 @@ export PGUSER=postgres
 psql <<- EOSQL
     CREATE USER engine WITH PASSWORD '$ENGINE_POSTGRES_PASSWORD';
     CREATE DATABASE engine;
+    CREATE DATABASE engine_test;
     GRANT ALL PRIVILEGES ON DATABASE engine TO engine;
+    GRANT ALL PRIVILEGES ON DATABASE engine_test TO engine;
 EOSQL
