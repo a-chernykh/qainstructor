aeb114c232f508d494a14d954a992c355399b84c
Andrey Chernih <andrey.chernih@gmail.com>
2015-12-30 11:59:46 -0800

    Updates

--- bin/prepare-production.sh
+++ bin/prepare-production.sh
@@ -12,7 +12,10 @@ bin/production.sh build selenium-server
 bin/production.sh build sample-app
 
 bin/production.sh build engine
+
 docker volume create --name bundle
+# Do not remove the intermediate container with --rm flag intentionally because it will remove the mounted volume
+# https://github.com/docker/docker/pull/16809
 docker run -v bundle:/bundle -u root qainstructor_engine /bin/bash -c 'mkdir -p /bundle/cache && chown -R app /bundle'
 bin/production.sh run --rm engine bundle install
 
@@ -20,6 +23,8 @@ echo 'Precompiling assets, hold on'
 bin/production.sh run --rm engine bundle exec rake assets:precompile
 
 bin/production.sh build rails
+
+mkdir /jobs
 bin/production.sh build sidekiq
 
 bin/production.sh build nginx
