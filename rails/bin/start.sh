22f771b2c7481077173eef251622557b30716009
Andrey Chernih <andrey.chernih@gmail.com>
2016-04-30 13:08:32 -0700

    Proper shutdown for rails container

--- rails/bin/start.sh
+++ rails/bin/start.sh
@@ -2,5 +2,23 @@
 
 set -e
 
+shutdown() {
+  pid=`ps -ef|grep rails|grep -v grep|awk '{ print $2 }'`
+  kill -SIGTERM $pid
+  wait $pid
+  running=0
+}
+
 bundle check || bundle install
-bundle exec rails server -b 0.0.0.0 -p 8080
+
+running=1
+trap "shutdown" SIGTERM
+bundle exec rails server -b 0.0.0.0 -p 8080 &
+
+while true; do
+  if [ "$running" = "1" ]; then
+    sleep 1
+  else
+    break
+  fi
+done
