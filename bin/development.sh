9d111bdfc55e03b9eced4187e98c34a1140ae810
Andrey Chernih <andrey.chernih@gmail.com>
2016-09-11 16:55:11 -0700

    Set docker-compose project name excplicitly

--- bin/development.sh
+++ bin/development.sh
@@ -4,4 +4,4 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker-compose -f $DIR/../docker-compose.yml -f $DIR/../docker-compose.development.yml "$@"
+docker-compose -p qainstructor -f $DIR/../docker-compose.yml -f $DIR/../docker-compose.development.yml "$@"
--- bin/production.sh
+++ bin/production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose.common.yml -f docker-compose.production.yml "$@"
+docker-compose -p qainstructor -f docker-compose.yml -f docker-compose.common.yml -f docker-compose.production.yml "$@"
